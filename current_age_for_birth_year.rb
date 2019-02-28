f188a16554df128d32f30ab0c0a279d59e7d8b48
Carlotta Junin <carlotta.junin@gmail.com>
2019-02-28 23:09:33 +0000

    Automatically backed up by Learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -1,5 +1,4 @@
 require_relative '../current_age_for_birth_year.rb'
-
  describe "current_age_for_birth_year method" do
   it "returns the age of a person based on the year of birth" do
     age_of_person = current_age_for_birth_year(1984)
@@ -8,6 +7,7 @@ require_relative '../current_age_for_birth_year.rb'
 
   end
 end
+
 it "should return the current year for a person born in year 0" do
   age = current_age_for_birthday_year(0)
 
